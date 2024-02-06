86c44168662ec9990e396893d91d6c61ffbaea15
Saurab Prasai <saurablovetocode@gmail.com>
2024-02-06 20:45:50 +0545

    Mongodb Connected

--- api/index.js
+++ api/index.js
@@ -1,8 +1,21 @@
+
 import express from "express";
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const app = express();
 
+mongoose.connect(process.env.MONGO_URI_STRING)
+.then(()=>{
+    console.log("MongoDb Connected");
+})
+.catch((err)=>console.log(err))
 
 app.listen(3000, () => {
   console.log("Listening on port 3000!");
 });
+
+
+
