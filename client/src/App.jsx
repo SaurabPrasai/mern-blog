516c6762aeb6f5d74c2e70393529e5954417abfb
Saurab Prasai <saurablovetocode@gmail.com>
2024-02-18 22:48:19 +0545

    make dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Signin from "./pages/Signin";
@@ -7,8 +7,10 @@ import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import Header from "./components/Header";
 import FooterComponent from "./components/Footer";
+import { useSelector } from "react-redux";
 
 export default function App() {
+  const {currentUser}=useSelector(state=>state.user)
   return (
     <BrowserRouter>
     <Header/>
@@ -17,7 +19,7 @@ export default function App() {
         <Route path="/about" element={<About/>}/>
         <Route path="/signin" element={<Signin/>}/>
         <Route path="/signup" element={<Signup/>}/>
-        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/dashboard" element={currentUser?<Dashboard/>:<Navigate to={'/signin'}/>}/>
         <Route path="/projects" element={<Projects/>}/>
       </Routes>
      <FooterComponent/>
