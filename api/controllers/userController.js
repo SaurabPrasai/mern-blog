9df70458f2542527a400c7765f48a9aa8b1b15ea
Saurab Prasai <saurablovetocode@gmail.com>
2024-02-29 13:30:37 +0545

    completed profile page

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -14,30 +14,33 @@ export const updateUser = async (req, res, next) => {
     if (req.body.password.length < 6) {
       return next(errorHandler(400, "Password must be atleast 6 characters"));
     }
-  }
   req.body.password = bcrypt.hashSync(req.body.password, 10);
-  if (req.body.username.length < 7 || req.body.username.length > 20) {
-    return next(
-      errorHandler(400, "Username must be between 7 and 20 characters")
-    );
   }
-  if(req.body.username.includes(" ")){
-    return next(errorHandler(400,"Username cannot contain spaces!"));
+  if (req.body.username) {
+    if (req.body.username.length < 7 || req.body.username.length > 20) {
+      return next(
+        errorHandler(400, "Username must be between 7 and 20 characters")
+      );
+    }
+    if (req.body.username.includes(" ")) {
+      return next(errorHandler(400, "Username cannot contain spaces!"));
+    }
   }
   try {
-    const updatedUser = await User.findByIdAndUpdate(
-      req.params.userId,
-      {
-        username: req.body.username,
-        email: req.body.email,
-        profilePicture: req.body.profilePicture,
-        password: req.body.password,
+    const updatedUser = await User.findByIdAndUpdate(req.params.userId,{
+      $set:{
+        username:req.body.username,
+        email:req.body.email,
+        password:req.body.password,
+        profilePicture:req.body.profilePicture
       },
-      {
-        new: true,
-      }
+    },{
+      new:true
+    }
     );
-    const { password, ...rest } = updatedUser;
+    const { password, ...rest } = updatedUser._doc;
     return res.status(200).json(rest);
-  } catch (error) {}
+  } catch (error) {
+  return next(errorHandler(400,error))
+  }
 };
--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,6 +1,7 @@
 import { Alert, Button, TextInput } from "flowbite-react";
 import { useState, useRef, useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { updateStart,updateSuccess,updateFailure } from "../redux/user/userSlice";
 import {
   getDownloadURL,
   getStorage,
@@ -8,14 +9,17 @@ import {
   uploadBytesResumable,
 } from "firebase/storage";
 import { app } from "../firebase";
-import { CircularProgressbar } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 export default function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
+  const dispatch=useDispatch()
   const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
-  const [imageFileUploadingProcess, setImageFileUploadingProcess] = useState(null);
+  const [imageFileUploadingProcess, setImageFileUploadingProcess] =
+    useState(null);
   const [imageFileUploadError, setImageFileUploadError] = useState(null);
+  const [formData, setFormData] = useState({});
   const fileRef = useRef();
   const handleImageChange = (e) => {
     const file = e.target.files[0];
@@ -49,23 +53,57 @@ export default function DashProfile() {
         setImageFileUploadError(
           "Could not upload image (File must be less than 2MB)"
         );
-        setImageFileUploadingProcess(null)
-        setImageFile(null)
-        setImageFileUrl(null)
+        setImageFileUploadingProcess(null);
+        setImageFile(null);
+        setImageFileUrl(null);
       },
       () => {
         getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
           setImageFileUrl(downloadUrl);
-          setImageFileUploadError(null)
+          setImageFileUploadError(null);
+          setFormData({ ...formData, profilePicture: downloadUrl });
         });
       }
     );
   };
 
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (Object.keys(formData).length === 0) {
+      return;
+    }
+    try {
+      dispatch(updateStart())
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+    const data=await res.json()
+    console.log(data);
+    if(!res.ok){
+     dispatch(updateFailure(data.message))
+    }else{
+      dispatch(updateSuccess(data))
+    }
+    } catch (error) {
+      dispatch(updateFailure(error.message))
+    }
+  
+
+  };
+
+  console.log(formData);
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">profile</h1>
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <input
           type="file"
           id=""
@@ -78,27 +116,34 @@ export default function DashProfile() {
           className="relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full"
           onClick={() => fileRef.current.click()}
         >
-          {imageFileUploadingProcess && <CircularProgressbar value={imageFileUploadingProcess||0}
-          text={`${imageFileUploadingProcess}%`}
-          strokeWidth={5}
-          styles={{
-            root:{
-              width:'100%',
-              height:"100%",
-              position:'absolute',
-              top:0,
-              left:0
-            },
-            path:{
-              stroke:`rgba(2,152,199,${imageFileUploadingProcess/100})`
-            }
-          }}
-          />}
+          {imageFileUploadingProcess && (
+            <CircularProgressbar
+              value={imageFileUploadingProcess || 0}
+              text={`${imageFileUploadingProcess}%`}
+              strokeWidth={5}
+              styles={{
+                root: {
+                  width: "100%",
+                  height: "100%",
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                },
+                path: {
+                  stroke: `rgba(2,152,199,${imageFileUploadingProcess / 100})`,
+                },
+              }}
+            />
+          )}
           <img
             src={imageFileUrl || currentUser.profilePicture}
             alt="user"
             className={`rounded-full w-full h-full object-cover border-8 border-[lightgray]
-            ${imageFileUploadingProcess && imageFileUploadingProcess<100 && 'opacity-60'}
+            ${
+              imageFileUploadingProcess &&
+              imageFileUploadingProcess < 100 &&
+              "opacity-60"
+            }
             `}
           />
         </div>
@@ -111,14 +156,21 @@ export default function DashProfile() {
           id="username"
           placeholder="username"
           defaultValue={currentUser.username}
+          onChange={handleChange}
         />
         <TextInput
           type="email"
           id="email"
           placeholder="email"
           defaultValue={currentUser.email}
+          onChange={handleChange}
         />
-        <TextInput type="password" id="password" placeholder="password" />
+        <TextInput
+          type="password"
+          id="password"
+          placeholder="password"
+          onChange={handleChange}
+        />
         <Button type="submit" gradientDuoTone={"purpleToBlue"} outline>
           Update
         </Button>
